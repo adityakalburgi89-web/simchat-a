162cae1c5f9e6d11434e75b15775d28f5d04e7f5
Aditya Kalburgi <adityakalburgi304@gmail.com>
2026-09-01 11:05:15 +0530

    feat: add main spring boot application entry point

---
+++ src/main/java/com/SIMCHAT_A/SIMCHAT_A/SimchatAApplication.java
@@ -0,0 +1,13 @@
+package com.SIMCHAT_A.SIMCHAT_A;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimchatAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimchatAApplication.class, args);
+	}
+
+}
