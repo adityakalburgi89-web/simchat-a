ada3a6fabc22b2148ad93bcedadf07682bbf9cf5
Aditya Kalburgi <adityakalburgi304@gmail.com>
2026-09-04 19:00:10 +0530

    exception: add ChatRoomNotFound runtime exception

---
+++ src/main/java/com/SIMCHAT_A/SIMCHAT_A/exception/ChatRoomNotFound.java
@@ -0,0 +1,10 @@
+package com.SIMCHAT_A.SIMCHAT_A.exception;
+
+public class ChatRoomNotFound extends RuntimeException {
+
+    public ChatRoomNotFound(String message) {
+        super(message);
+
+    }
+
+}
