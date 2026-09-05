7dcfd87394db1999da49169b69fa6f0b3548689b
Aditya Kalburgi <adityakalburgi304@gmail.com>
2026-09-05 11:20:00 +0530

    exception: add DuplicateChatRoom exception

---
+++ src/main/java/com/SIMCHAT_A/SIMCHAT_A/exception/DuplicateChatRoom.java
@@ -0,0 +1,8 @@
+package com.SIMCHAT_A.SIMCHAT_A.exception;
+
+public class DuplicateChatRoom extends RuntimeException {
+
+    public DuplicateChatRoom(String message) {
+        super(message);
+    }
+}
