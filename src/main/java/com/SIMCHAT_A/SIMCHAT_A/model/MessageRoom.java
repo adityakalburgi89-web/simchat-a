7c6fd26bc50e3faa69832ba29867be2aba7c15f8
Aditya Kalburgi <adityakalburgi304@gmail.com>
2026-09-11 19:57:21 +0530

    refactor(model): update MessageRoom entity fields to participant and string timestamp

--- src/main/java/com/SIMCHAT_A/SIMCHAT_A/model/MessageRoom.java
+++ src/main/java/com/SIMCHAT_A/SIMCHAT_A/model/MessageRoom.java
@@ -1,22 +1,16 @@
 package com.SIMCHAT_A.SIMCHAT_A.model;
 
-import java.time.LocalDateTime;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-
 
 @Data 
 @NoArgsConstructor 
 @AllArgsConstructor 
 @Builder 
 public class MessageRoom {
-
-    private String username;
+    private String participant;
     private String message;
-    private LocalDateTime timestamp;
-    
+    private String timestamp;
 }
